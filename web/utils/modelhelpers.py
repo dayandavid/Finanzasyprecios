836822052804dd0b307a15602175d5d313406910
dayandavid <33235380+dayandavid@users.noreply.github.com>
2022-05-01 14:48:11 -0400

    Letra a mayuscula

--- web/utils/modelhelpers.py
+++ web/utils/modelhelpers.py
@@ -6,7 +6,7 @@ class TimeStampedModel(models.Model):
         auto_now_add=True, editable=False, verbose_name='Fecha de Creación')
 
     updated_at = models.DateTimeField(
-        auto_now=True, editable=False, verbose_name='Útima actualización')
+        auto_now=True, editable=False, verbose_name='Útima Actualización')
 
     class Meta:
         abstract = True
